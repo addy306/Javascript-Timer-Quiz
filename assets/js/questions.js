51902dcac5aad57489b6258da4e60f3712ae0d58
Your Name <you@example.com>
2023-12-11 22:31:26 +0000

    Adding questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,58 @@
+var questions = [
+    {
+        question: "What is the typeof operator used for in JavaScript?",
+        options: ["Check the data type of a variable", 
+        "Check if a variable is defined", 
+        "Convert a variable to a specific type"],
+        correctAnswer: "Check the data type of a variable"
+    },
+    {
+        question: "Which keyword is used to declare a variable in JavaScript?",
+        options: ["let", "var", "const"],
+        correctAnswer: "let"
+    },
+    {
+        question: "What does the acronym 'DOM' stand for?",
+        options: ["Document Object Model", "Data Object Model", "Digital Output Module"],
+        correctAnswer: "Document Object Model"
+    },
+    {
+        question: "How do you comment in JavaScript?",
+        options: ["<!-- This is a comment -->", 
+        "// This is a comment", "/** This is a comment */"],
+        correctAnswer: "// This is a comment"
+    },
+    {
+        question: "What is the purpose of the 'addEventListener' method in JavaScript?",
+        options: ["Create a new element",
+         "Attach an event handler to an element", "Remove an event from an element"],
+        correctAnswer: "Attach an event handler to an element"
+    },
+    {
+        question: "Which method is used to remove the last element from an array in JavaScript?",
+        options: ["pop()", "removeLast()", "deleteLast()"],
+        correctAnswer: "pop()"
+    },
+    {
+        question: "What does the 'NaN' stand for in JavaScript?",
+        options: ["Not a Number", "No and Null", "Negative and Null"],
+        correctAnswer: "Not a Number"
+    },
+    {
+        question: "What is the purpose of 'JSON.stringify()' in JavaScript?",
+        options: ["Parse a JSON string", 
+        "Convert a JavaScript object to a JSON string", 
+        "Convert a JSON string to a JavaScript object"],
+        correctAnswer: "Convert a JavaScript object to a JSON string"
+    },
+    {
+        question: "Which operator is used for strict equality in JavaScript?",
+        options: ["=", "==", "==="],
+        correctAnswer: "==="
+    },
+    {
+        question: "What does the 'this' keyword refer to in JavaScript?",
+        options: ["The current function", "The parent object", "The current instance of an object"],
+        correctAnswer: "The current instance of an object"
+    }
+];
